940f4211a1129bd4ddf5e9c058c5d91ae666db35
Ilker Cakar <ilker.cakar@interabrasive.com>
2022-11-26 10:44:41 +0300

    initial commit

---
+++ src/main/java/com/bt_akademi/user_management/model/entity/User.java
@@ -0,0 +1,9 @@
+package com.bt_akademi.user_management.model.entity;
+
+public class User
+{
+    ID,
+    USER_NAME,
+    DATE,
+
+}
---
+++ src/main/java/com/bt_akademi/user_management/model/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package com.bt_akademi.user_management.model.repository;
+
+public interface UserRepository
+{
+}
